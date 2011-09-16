263d5804f14ea8fdaf26a243c678ddca9b54100a
Sergei O. Udalov <sergei.udalov@gmail.com>
2011-09-17 00:59:14 +0400

    team title -> name

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,6 @@
 class Team < ActiveRecord::Base
+  has_many :home_matches, :class_name => "Match", :foreign_key => :team_1_id
+  has_many :out_matches, :class_name => "Match", :foreign_key => :team_2_id
+
+  validate :name, :presence => true
 end
--- db/migrate/20110916204502_create_teams.rb
+++ db/migrate/20110916204502_create_teams.rb
@@ -1,7 +1,7 @@
 class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
-      t.string :title
+      t.string :name
 
       t.timestamps
     end
