df0ff24e18dee2a80ca5067154e90e8c163c9f10
Sergei O. Udalov <sergei.udalov@gmail.com>
2011-09-17 00:11:19 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watchme::Application.config.secret_token = 'e62c02f0f231d1e5ba137f326d319955fb9312d2eec8f9e9b7edcc2e508d728a7dee312e6a46ff218357a024e5189c3e85bdc28fffbff66d3b735818043c5849'
