38ca221101bfb65253791d7109527a7630d94afc
Sergei O. Udalov <sergei.udalov@gmail.com>
2011-10-25 11:17:01 +0400

    smart sql

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,6 +3,6 @@ class MatchesController < InheritedResources::Base
   private
 
   def collection
-    @matches ||= Match.recent.order("rating desc, began_on desc")
+    @matches ||= Match.recent.order("rating desc, began_on desc").includes(:team_1, :team_2, :league)
   end
 end
